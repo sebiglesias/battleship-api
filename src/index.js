64bbe18ab64ea3a1f39841aab255cf518e37ac90
tomifor <tomas_forman@outlook.com>
2018-05-11 19:21:03 -0300

    socket working

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3000;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
+    socket.on('message', function(msg){
         console.log(msg);
-        io.emit('chat message', msg);
+        // io.emit('chat message', msg);
     });
+
+
 });
 
 http.listen(port, function(){
